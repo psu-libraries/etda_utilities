3ee08f7f85b95e46e98c889337c6d790b3d39e22
Alex Kiessling <32677188+ajkiessl@users.noreply.github.com>
2022-06-09 13:49:53 -0400

    Refactor Partner class (#19)
    * Moves shared partner methods from workflow and explore into EtdaUtilities::Partner class.
    
    * Rubocop

--- spec/lib/etda_utilities_partner_spec.rb
+++ spec/lib/etda_utilities_partner_spec.rb
@@ -1,77 +1,158 @@
 # frozen_string_literal: true
 
 RSpec.describe EtdaUtilities::Partner, type: :model do
-  describe 'current_partner.id' do
-    context "when ENV['PARTNER'] is graduate" do
-      before do
-        partner_set_env('graduate')
-      end
+  context "when ENV['PARTNER'] is graduate" do
+    let(:current_partner) { described_class.current }
 
-      it 'sets current.id to graduate' do
-        expect(described_class.current.id).to eql('graduate')
-      end
-      it 'responds to graduate?' do
-        expect(described_class.current).to be_graduate
-      end
+    before do
+      partner_set_env('graduate')
     end
 
-    context "when ENV['PARTNER'] is honors" do
-      before do
-        partner_set_env('honors')
-      end
+    it 'sets current.id to graduate' do
+      expect(current_partner.id).to eql('graduate')
+    end
+    it 'responds to graduate?' do
+      expect(current_partner).to be_graduate
+    end
+    it 'returns partner name' do
+      expect(current_partner.name).to eq("Graduate School")
+    end
+    it 'returns partner email' do
+      expect(current_partner.email_address).to eq("gradthesis@psu.edu")
+    end
+    it 'returns partner email list' do
+      expect(current_partner.email_list).to eq('ul-etda-graduate-admin@pennstateoffice365.onmicrosoft.com')
+    end
+    it 'returns partner slug' do
+      expect(current_partner.slug).to eq("eTD")
+    end
+    it 'returns partner label' do
+      expect(current_partner.program_label).to eq("Graduate Program")
+    end
+    it 'returns partner committee label' do
+      expect(current_partner.committee_label).to eq("Committee Member")
+    end
+    it 'returns partner committee list label' do
+      expect(current_partner.committee_list_label).to eq("Committee Members")
+    end
+  end
 
-      it 'sets current.id to honors' do
-        expect(described_class.current.id).to eql('honors')
-      end
-      it 'responds to honors?' do
-        expect(described_class.current).to be_honors
-      end
+  context "when ENV['PARTNER'] is honors" do
+    let(:current_partner) { described_class.current }
+
+    before do
+      partner_set_env('honors')
     end
 
-    context "when ENV['PARTNER'] is milsch" do
-      before do
-        partner_set_env('milsch')
-      end
+    it 'sets current.id to honors' do
+      expect(current_partner.id).to eql('honors')
+    end
+    it 'responds to honors?' do
+      expect(current_partner).to be_honors
+    end
+    it 'returns partner name' do
+      expect(current_partner.name).to eq("Schreyer Honors College")
+    end
+    it 'returns partner email' do
+      expect(current_partner.email_address).to eq("SHCAcademics@psu.edu")
+    end
+    it 'returns partner email list' do
+      expect(current_partner.email_list).to eq('ul-etda-honors-admin@pennstateoffice365.onmicrosoft.com')
+    end
+    it 'returns partner slug' do
+      expect(current_partner.slug).to eq("eHT")
+    end
+    it 'returns partner label' do
+      expect(current_partner.program_label).to eq("Area of Honors")
+    end
+    it 'returns partner committee label' do
+      expect(current_partner.committee_label).to eq("Thesis Supervisor")
+    end
+    it 'returns partner committee list label' do
+      expect(current_partner.committee_list_label).to eq("Thesis Supervisors")
+    end
+  end
 
-      it 'sets current.id to milsch' do
-        expect(described_class.current.id).to eql('milsch')
-      end
-      it 'responds to milsch?' do
-        expect(described_class.current).to be_milsch
-      end
+  context "when ENV['PARTNER'] is milsch" do
+    let(:current_partner) { described_class.current }
+
+    before do
+      partner_set_env('milsch')
     end
 
-    context "when ENV['PARTNER'] is sset" do
-      before do
-        partner_set_env('sset')
-      end
+    it 'sets current.id to milsch' do
+      expect(current_partner.id).to eql('milsch')
+    end
+    it 'responds to milsch?' do
+      expect(current_partner).to be_milsch
+    end
+    it 'returns partner name' do
+      expect(current_partner.name).to eq("Millennium Scholars Program")
+    end
+    it 'returns partner email' do
+      expect(current_partner.email_address).to eq("millennium@psu.edu")
+    end
+    it 'returns partner email list' do
+      expect(current_partner.email_list).to eq('ul-etda-milsch-admin@pennstateoffice365.onmicrosoft.com')
+    end
+    it 'returns partner slug' do
+      expect(current_partner.slug).to eq("MSPT")
+    end
+    it 'returns partner label' do
+      expect(current_partner.program_label).to eq("Millennium Scholars Program Name")
+    end
+    it 'returns partner committee label' do
+      expect(current_partner.committee_label).to eq("Thesis Supervisor")
+    end
+    it 'returns partner committee list label' do
+      expect(current_partner.committee_list_label).to eq("Thesis Supervisor")
+    end
+  end
 
-      it 'sets current.id to sset' do
-        expect(described_class.current.id).to eql('sset')
-      end
-      it 'responds to sset?' do
-        expect(described_class.current).to be_sset
-      end
+  context "when ENV['PARTNER'] is sset" do
+    let(:current_partner) { described_class.current }
+
+    before do
+      partner_set_env('sset')
     end
 
-    context "when ENV['PARTNER'] is not a valid partner" do
-      before do
-        partner_set_env('boguspartner')
-      end
+    it 'sets current.id to sset' do
+      expect(current_partner.id).to eql('sset')
+    end
+    it 'responds to sset?' do
+      expect(current_partner).to be_sset
+    end
+    it 'returns partner name' do
+      expect(current_partner.name).to eq("School of Science, Engineering, and Technology")
+    end
+    it 'returns partner email' do
+      expect(current_partner.email_address).to eq("sset@psu.edu")
+    end
+    it 'returns partner email list' do
+      expect(current_partner.email_list).to eq('ul-etda-sset-admin@pennstateoffice365.onmicrosoft.com')
+    end
+    it 'returns partner slug' do
+      expect(current_partner.slug).to eq("SSETT")
+    end
+  end
 
-      it 'reports an error' do
-        expect { described_class.current.id }.to raise_error(ArgumentError)
-      end
+  context "when ENV['PARTNER'] is not a valid partner" do
+    before do
+      partner_set_env('boguspartner')
     end
 
-    context "when ENV['PARTNER'] is not set" do
-      before do
-        ENV.delete('PARTNER')
-      end
+    it 'reports an error' do
+      expect { described_class.current.id }.to raise_error(ArgumentError)
+    end
+  end
 
-      it 'reports an error' do
-        expect { described_class.current.id }.to raise_error(KeyError)
-      end
+  context "when ENV['PARTNER'] is not set" do
+    before do
+      ENV.delete('PARTNER')
+    end
+
+    it 'reports an error' do
+      expect { described_class.current.id }.to raise_error(KeyError)
     end
   end
 end
